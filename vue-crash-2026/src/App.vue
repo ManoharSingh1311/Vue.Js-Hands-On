22b89802f8631eaf196c882dbf04800cf6e7aad5
Manohar Singh <burathimannu@gmail.com>
2026-06-29 21:20:19 +0530

    new changes made

---
+++ vue-crash-2026/src/App.vue
@@ -0,0 +1,42 @@
+<script setup >
+
+import {ref} from 'vue';
+// Composition API
+
+    const name = ref('John Doe');
+    const status = ref('active');
+    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+
+    const toggleStatus = () => {
+      if (status.value === 'active') {
+        status.value = 'pending';
+      } else if (status.value === 'pending') {
+        status.value = 'inactive';
+      } else {
+        status.value = 'active';
+      }
+    }; 
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status === 'pending'">User is Pending</p>
+  <p v-else>User is Inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{ task }}</li>
+  </ul>
+
+  <a :href="link">Click for Google!</a>
+  <br>
+
+  <button @click="toggleStatus">Change Status</button>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
